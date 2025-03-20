1f95a81fadd0c140a9caa77f93510766fd76551f
이지은 <wlsl8302@naver.com>
2025-03-20 16:45:17 +0900

    :truck: Chore: YAML 파일 이름 변경 및 import 경로 수정

--- gbh_app/lib/core/widgets/text_input.dart
+++ gbh_app/lib/core/widgets/text_input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:test0316_1/core/theme/app_colors.dart';
-import 'package:test0316_1/core/theme/app_text_styles.dart';
+import 'package:marshmellow/core/theme/app_colors.dart';
+import 'package:marshmellow/core/theme/app_text_styles.dart';
 
 class TextInput extends StatefulWidget {
   final String label;

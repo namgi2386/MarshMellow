13d8a3e4714814498830762f678b34920b5b9b75
yuhanseul <yuhanseul5@gmail.com>
2025-03-22 14:00:34 +0900

    :sparkles: Feat: 상단바 #226

---
+++ gbh_app/lib/presentation/widgets/custom_appbar/custom_appbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:marshmellow/core/theme/app_colors.dart';
+import 'package:marshmellow/core/theme/app_text_styles.dart';
+
+/*
+  상단 커스텀 앱바 UI
+*/
+class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget> actions;
+  final VoidCallback? onNofiticationTap;
+  final Color backgroundColor;
+  final bool hideNotificaiotnIcon; // 알림페이지에서는 알림 아이콘을 표시하지 않을겁니다
+
+  const CustomAppbar({
+    Key? key,
+    required this.title,
+    this.actions = const [],
+    this.onNofiticationTap,
+    this.backgroundColor = AppColors.background,
+    this.hideNotificaiotnIcon = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: backgroundColor,
+      title: Text(
+        title,
+        style: AppTextStyles.appBar
+      ),
+      actions: [
+        // 아이콘을 추가할 거라면 여기에 넣으세요
+        ...actions,
+        // 알림 아이콘 우측 고정
+        if (!hideNotificaiotnIcon) IconButton(
+          icon: Icon(Icons.notifications_none),
+          constraints: BoxConstraints(),
+          padding: const EdgeInsets.only(left: 0, right: 12),
+          // 누르면 알림 페이지로 이동
+          onPressed: onNofiticationTap ?? () {
+            GoRouter.of(context).push('/notification');
+          }, 
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+

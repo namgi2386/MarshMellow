1ab40aa0fd001e6953e1bf2e4e7ee6470b2a7dc2
이지은 <wlsl8302@naver.com>
2025-03-24 11:31:37 +0900

    :sparkles: Feat: 검색 바 추가 #108-302
    Former-commit-id: 82aa1e6d0e59a3fce02c303868f82c1f907533ba

---
+++ gbh_app/lib/presentation/widgets/custom_search_bar/custom_search_bar.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:marshmellow/presentation/widgets/round_input/round_input.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final ValueChanged<String>? onChanged;
+  final String? hintText;
+  final VoidCallback? onSearchPressed;
+
+  const CustomSearchBar({
+    Key? key,
+    required this.controller,
+    this.onChanged,
+    this.hintText = '검색어를 입력하세요',
+    this.onSearchPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final containerWidth = MediaQuery.of(context).size.width * 0.9;
+
+    return Container(
+      width: containerWidth,
+      height: 50,
+      child: Stack(
+        children: [
+          // RoundInput을 기본 배경으로 사용
+          Positioned(
+            left: 0,
+            top: 0,
+            child: Container(
+              width: containerWidth * 0.85,
+              child: RoundInput(
+                width: containerWidth * 0.85,
+                controller: controller,
+                onChanged: onChanged,
+                hintText: hintText,
+                showDropdown: false,
+              ),
+            ),
+          ),
+
+          // 검색 아이콘
+          Positioned(
+            right: 0,
+            top: 6.5,
+            child: GestureDetector(
+              onTap: onSearchPressed,
+              child: SvgPicture.asset(
+                'assets/icons/search_bar/search_button.svg',
+                width: 40,
+                height: 40,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

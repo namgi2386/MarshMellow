a0b01fb6f04b7828119390728d685ede0ac0afc4
yunjaeeun <yunjaeeun12@naver.com>
2025-03-28 17:56:33 +0900

    :bug: Fix: 오류 수정
    Contructror 추가
    
    
    Former-commit-id: c7113fc0faa7457f1d9309380fe36f0f55ab618d

--- gbh_mm/src/main/java/com/gbh/gbh_mm/household/component/AutoTransactionComponent.java
+++ gbh_mm/src/main/java/com/gbh/gbh_mm/household/component/AutoTransactionComponent.java
@@ -15,6 +15,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @AllArgsConstructor
 public class AutoTransactionComponent {

b46d3420333734bef8ab17b20ebfae8d2e06c086
yuhanseul <yuhanseul5@gmail.com>
2025-04-09 11:11:16 +0900

    :sparkles: Feat: 알림 아이콘 설정
    Former-commit-id: 9a38ee3ee24df7196c8c1e9c7b483934bad597fe

--- gbh_app/lib/presentation/pages/auth/auth_check_page.dart
+++ gbh_app/lib/presentation/pages/auth/auth_check_page.dart
@@ -41,11 +41,11 @@ class _AuthCheckPageState extends ConsumerState<AuthCheckPage> {
     await secureStorage.write(
         key: StorageKeys.accessToken,
         value:
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlblR5cGUiOiJBQ0NFU1MiLCJ1c2VyUGsiOjMsInN1YiI6ImFjY2Vzcy10b2tlbiIsImlhdCI6MTc0NDEzNjcyNCwiZXhwIjoxNzQ0MTU0NzI0fQ.RtbdBg-N2ZrFd6TZIpedYuXDl7xz8NtXim4gbVBaXrKpTNO8-GPHM_6Zv1nC2WBeIYvQsXpf0GQGSWSHrh0tgg');
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlblR5cGUiOiJBQ0NFU1MiLCJ1c2VyUGsiOjMsInN1YiI6ImFjY2Vzcy10b2tlbiIsImlhdCI6MTc0NDE2NDI2NCwiZXhwIjoxNzQ0MTgyMjY0fQ.btvUeF60YCgAOVoN3ShiqpfAwW6aoMHL_7BYmfzR-9d3WEfXKR7DqoikCP6yZF2zZk8Vu86GpoBcbUs2XGzR9A');
     await secureStorage.write(
         key: StorageKeys.refreshToken,
         value:
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlblR5cGUiOiJSRUZSRVNIIiwidXNlclBrIjozLCJzdWIiOiJyZWZyZXNoLXRva2VuIiwiaWF0IjoxNzQ0MTM2NzI0LCJleHAiOjE3NzAwNTY3MjR9.RNu8WIajYZHmXQDB43o_3dJYkEro6ex-B6X9Ex9mIjGOevKSODeuc8rXd1fCdBf9lBmt4ztriyCTWJjWYYwQyw');
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlblR5cGUiOiJSRUZSRVNIIiwidXNlclBrIjozLCJzdWIiOiJyZWZyZXNoLXRva2VuIiwiaWF0IjoxNzQ0MTY0MjY0LCJleHAiOjE3NzAwODQyNjR9.IQPy8To2Ed8RmPQGwt4TS4QtBUeiFj3qRf6P5csQWMD1aEgES3rgTcDRobppmfugTjKXw4feTpAhr_x2NHfdVQ');
     await secureStorage.write(
         key: StorageKeys.certificatePem,
         value:
